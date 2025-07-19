ec512eb88cfef2f23c0aee3ded213468e75975d0
Sherly <mac@siMAC-13.local>
2025-07-19 12:14:06 +0800

    feat: added Hand to Home Greetings and animation on entry

--- src/components/HomeGreetings.jsx
+++ src/components/HomeGreetings.jsx
@@ -1,10 +1,12 @@
 import greetings from "../assets/HomeGreetings.png";
+import hand from "../assets/greetingHand.png";
 import styles from "./styles/HomeGreetings.module.scss";
 
 export default function HomeGreetings() {
     return (
         <div className={styles.HomeGreetings}>
             <img src={greetings} alt="" className={styles.greeting} />
+            <img src={hand} alt="" className={styles.hand} />
         </div>
     );
 }
