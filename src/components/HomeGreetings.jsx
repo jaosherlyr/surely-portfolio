da13ded519045d88b4e12c5fb334a9ee1f595eab
Sherly <mac@siMAC-13.local>
2025-07-19 13:03:06 +0800

    feat: added a handleContextMenu to homegreetings

--- src/components/HomeGreetings.jsx
+++ src/components/HomeGreetings.jsx
@@ -1,12 +1,12 @@
-import greetings from "../assets/HomeGreetings.png";
 import hand from "../assets/greetingHand.png";
 import styles from "./styles/HomeGreetings.module.scss";
 
 export default function HomeGreetings() {
-    return (
-        <div className={styles.HomeGreetings}>
-            <img src={greetings} alt="" className={styles.greeting} />
-            <img src={hand} alt="" className={styles.hand} />
-        </div>
-    );
+  const handleContextMenu = (e) => e.preventDefault();
+
+  return (
+    <div className={styles.HomeGreetings} onContextMenu={handleContextMenu}>
+      <img src={hand} alt="" className={styles.hand} />
+    </div>
+  );
 }
