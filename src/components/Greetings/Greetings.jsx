7deaf2e506979bec6bd5357c026a2939f2ff8ac0
Sherly Jao <sherly@Sherlys-MacBook-Pro.local>
2025-08-06 20:33:33 +0800

    style: change unit in css

--- src/components/Greetings/Greetings.jsx
+++ src/components/Greetings/Greetings.jsx
@@ -9,8 +9,8 @@ export default function HomeGreetings() {
     <div className={styles.homeGreetings} onContextMenu={handleContextMenu}>
       <img src={hello} alt="" className={styles.hello} />
         <div className={styles.greetingTxt}>
-          <h1>Hi! I'm Sherly.</h1>
-          <h1>I'm a web developer and artist based in the Philippines.</h1>
+          <h1>Hi! I'm Sherly. <br /> 
+          I'm a web developer and artist based in the Philippines.</h1>
         </div>
 
       <img src={hand} alt="" className={styles.hand} />
