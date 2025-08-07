6fea3676797d4e23b18876dfafae28b2a53c4806
Sherly Jao <sherly@Sherlys-MacBook-Pro.local>
2025-08-07 19:46:40 +0800

    style: edited greetings component and added left container

--- src/components/Greetings/Greetings.jsx
+++ src/components/Greetings/Greetings.jsx
@@ -2,16 +2,18 @@ import hand from "../../assets/greetingHand.png";
 import hello from "../../assets/HomeGreetings.png";
 import styles from "./Greetings.module.scss";
 
-export default function HomeGreetings() {
+export default function Greetings() {
   const handleContextMenu = (e) => e.preventDefault();
 
   return (
     <div className={styles.homeGreetings} onContextMenu={handleContextMenu}>
-      <img src={hello} alt="" className={styles.hello} />
+      <div className={styles.leftContainerGreetings}>
+        <img src={hello} alt="" className={styles.hello} />
+
         <div className={styles.greetingTxt}>
-          <h1>Hi! I'm Sherly. <br /> 
-          I'm a web developer and artist based in the Philippines.</h1>
+          <h1>Hi! I'm Sherly. <br /> I'm a web developer and artist based in the Philippines.</h1>
         </div>
+      </div>
 
       <img src={hand} alt="" className={styles.hand} />
     </div>
