b6e2b4999b47871dcdca6d7233329b94657c1522
Sherly Jao <sherly.dev@gmail.com>
2025-08-22 13:31:36 +0800

    added hover height on Skill Section for cleaner empty space

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -5,7 +5,7 @@ import styles from "./Skills.module.scss";
 export default function Skills() {
   return (
     <div className={styles.skillSection}>
-      <h1>The Skills I've Learned.</h1>
+      <h1 className={styles.skillTitle}>The Skills I've Learned.</h1>
 
       <div className={styles.skillsList}>
         <SkillsList />
