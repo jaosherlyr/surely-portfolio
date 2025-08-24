1b2b830b7d90c8dacf34b2e6de4ab4b589c13532
Sherly Jao <sherly.dev@gmail.com>
2025-08-24 22:12:08 +0800

    style: made works a flex 4 item per row that centers

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import DesignsList from "../components/Lists/DesignsList";
 import IllustrationsList from "../components/Lists/IllustrationsList";
 import WebProjectsList from "../components/Lists/WebProjectsList";
@@ -5,23 +6,31 @@ import WebProjectsList from "../components/Lists/WebProjectsList";
 import styles from "./styles/Works.module.scss";
 
 export default function Works() {
+  const theme = useSelector((state) => state.theme.mode);
+
   return(
     <section id="works" className={styles.worksContainer}>
       <h1 className={styles.worksTitle}>Explore my Works!</h1>
 
-      <h2>Developments</h2>
-      <div className={styles.worksListContainer}>
-        <WebProjectsList/>
+      <div className={styles.subworkContainer}>
+        <h2 className={`${styles.worksSubtitle} ${theme === 'dark' ? styles.dark : ''}`}>Developments</h2>
+        <div className={styles.worksListContainer}>
+          <WebProjectsList/>
+        </div>
       </div>
 
-      <h2>UI/UX Designs</h2>
-      <div className={styles.worksListContainer}>
-        <DesignsList />
+      <div className={styles.subworkContainer}>
+        <h2 className={`${styles.worksSubtitle} ${theme === 'dark' ? styles.dark : ''}`}>UI/UX Designs</h2>
+        <div className={styles.worksListContainer}>
+          <DesignsList />
+        </div>
       </div>
-
-      <h2>Illustrations</h2>
-      <div className={styles.worksListContainer}>
-        <IllustrationsList />
+      
+      <div className={styles.subworkContainer}>
+        <h2 className={`${styles.worksSubtitle} ${theme === 'dark' ? styles.dark : ''}`}>Illustrations</h2>
+        <div className={styles.worksListContainer}>
+          <IllustrationsList />
+        </div>
       </div>
     </section>
   );
