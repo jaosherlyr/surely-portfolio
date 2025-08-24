36e2051abd7a20d3848c4b60125f0ca8c4c85fa2
Sherly Jao <sherly.dev@gmail.com>
2025-08-24 18:11:06 +0800

    test: added test images to work page

--- src/components/Lists/DesignsList.jsx
+++ src/components/Lists/DesignsList.jsx
@@ -2,26 +2,39 @@ import { useState } from "react";
 
 import WorkCard from "../Card/WorkCard";
 
+//TEST images
+import test1 from '../../assets/test1.jpg';
+import test2 from '../../assets/test2.jpg';
+import test3 from '../../assets/test3.jpg';
+import test4 from '../../assets/test4.jpg';
+
 export default function DesignsList() {
     const designsData = [
         {
             title: "Wakey-Wakey",
             timeline: "August 2023",
-            media: [],
+            media: [
+                test1
+            ],
             link: "",
             details: "The prototype for Wakey-Wakey was designed with both light and dark themes in mind, which I worked on using Figma. I came up with the idea of highlighting a clock as the centerpiece of the main page, allowing it to transition smoothly between analog and digital modes. It was a really fun and engaging project that not only let me explore creative design ideas but also helped me learn and grow in the process.",
         },
         {
             title: "Jip",
             timeline: "May 2023",
-            media: [],
+            media: [
+                test2
+            ],
             link: "",
             details: "This UI/UX design is one of my favorite projects, inspired by my daily commute in Cebu City. Since I’m not very familiar with all the jeepney routes, I wanted to create something that would’ve been really helpful for commuters like me. One of the main challenges is that there’s no official record of the jeepney routes, and they often change over time. Despite that, it’s still a project I’d love to revisit and develop further in the future.",
         },
         {
             title: "Witness",
             timeline: "May 2024",
-            media: [],
+            media: [
+                test4,
+                test1
+            ],
             link: "",
             details: "This was a group project where I led the creation of the prototype using Figma. The app was meant to address the problem of how even minor traffic incidents can cause major congestion in Cebu. Often, small accidents with no injuries can lead to long delays while everyone waits for traffic enforcers to arrive. Our idea was to give drivers the ability to quickly report such minor incidents through the app, helping reduce unnecessary traffic buildup. Of course, this concept would require responsible user reporting to work effectively, but it was an interesting exploration of how technology could ease everyday commuting challenges.",
 
--- src/components/Lists/IllustrationsList.jsx
+++ src/components/Lists/IllustrationsList.jsx
@@ -1,26 +1,43 @@
 import { useState } from "react";
 import WorkCard from "../Card/WorkCard";
 
+//TEST images
+import test1 from '../../assets/test1.jpg';
+import test2 from '../../assets/test2.jpg';
+import test3 from '../../assets/test3.jpg';
+import test4 from '../../assets/test4.jpg';
+
 export default function IllustrationsList() {
     const illustrationsData = [
         {
             title: "Hala Sombi!",
             timeline: "October 2023 - March 2024",
-            media: [],
+            media: [
+                test1,
+                test3,
+            ],
             link: "https://web.facebook.com/rjrzperalta/posts/pfbid02HPxnz2RgS2qCwRn68ePBm4H6MBmWBUCWbUyW29DN8PpPqMcKHSxejbhf52z1KF7sl",
             details: "This was my first experience illustrating a children’s book. At first, I took on the project simply as a way to earn some extra income, but I was pleasantly surprised by how much I enjoyed the process. The story follows a lively child who eventually falls asleep after playing without cleaning up his toys and the . I especially loved working on the cover art and character design.",
         },
         {
             title: "Beep… Boop… I am a Robot",
             timeline: "January 2024 - March 2024",
-            media: [],
+            media: [
+                test2,
+                test4,
+            ],
             link: "https://web.facebook.com/rjrzperalta/posts/pfbid02HPxnz2RgS2qCwRn68ePBm4H6MBmWBUCWbUyW29DN8PpPqMcKHSxejbhf52z1KF7sl",
             details: "This was the second children’s book I illustrated, commissioned by the same client as Hala Sombi. Unlike the first project, this one was a unique challenge for me since I had no prior experience drawing robots, especially ones that needed to look cute and approachable for children. I went through several iterations of character designs, experimenting with different looks and color schemes, before finalizing a design that was approved. It was a great learning experience, and I really enjoyed pushing myself outside of my usual style.",
         },
         {
             title: "Si Perlas, and iring sa isla Santa Fe (Perlas, the cat in the island of Santa Fe)",
             timeline: "April 2024",
-            media: [],
+            media: [
+                test1,
+                test2,
+                test3,
+                test4
+            ],
             link: "https://drive.google.com/file/d/1FxOJjwlr6X2ApG-v9OpS1ZVMJ4HR1c_W/view",
             details: "This was a group academic project where I took the lead in the design aspect, being the more artistic member of the team. We created an eBook as part of a partnership between the University of San Carlos (USC) and the National Commission for Culture and the Arts (NCCA). Thanks to this collaboration, our work became part of the Taga Sugbo Ako Digital Children’s Books on Cebuano Festivals, a collection of free digital books. It was a meaningful project that combined creativity, culture, and teamwork.",
 
