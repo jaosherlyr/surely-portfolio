98038b170e58aebf29f7d372b8fa318f21ec499a
Sherly Jao <sherly.dev@gmail.com>
2025-09-07 20:35:41 +0800

    added option features to works

---
+++ src/components/Works/WorksOption.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import styles from "./WorksOption.module.scss";
+
+export default function WorksOptions({ active, setActive }) {
+  const options = ["Website Development", "UI/UX Design", "Art Illustrations"];
+  const theme = useSelector((state) => state.theme.mode);
+
+  return (
+    <div className={styles.worksOptions}>
+      {options.map((opt) => (
+        <h3
+          key={opt}
+          className={`${styles.option} ${active === opt ? styles.active : ""} ${
+            theme === "dark" ? styles.dark : ""
+          }`}
+          onClick={() => {
+            console.log(`Clicked: ${opt}`);
+            setActive(opt);
+          }}
+        >
+          {opt}
+        </h3>
+      ))}
+    </div>
+  );
+}
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,36 +1,26 @@
-import { useSelector } from "react-redux";
 import DesignsList from "../components/Lists/DesignsList";
 import IllustrationsList from "../components/Lists/IllustrationsList";
 import WebProjectsList from "../components/Lists/WebProjectsList";
 
 import styles from "./styles/Works.module.scss";
+import { useState } from "react";
+import WorksOptions from "../components/Works/WorksOption";
 
 export default function Works() {
-  const theme = useSelector((state) => state.theme.mode);
+  const [active, setActive] = useState("default");
 
-  return(
+  return (
     <section id="works" className={styles.worksContainer}>
       <h1 className={styles.worksTitle}>Explore my Works!</h1>
 
-      <div className={styles.subworkContainer}>
-        <h2 className={`${styles.worksSubtitle} ${theme === 'dark' ? styles.dark : ''}`}>Developments</h2>
-        <div className={styles.worksListContainer}>
-          <WebProjectsList/>
-        </div>
-      </div>
+      <WorksOptions active={active} setActive={setActive} />
 
-      <div className={styles.subworkContainer}>
-        <h2 className={`${styles.worksSubtitle} ${theme === 'dark' ? styles.dark : ''}`}>UI/UX Designs</h2>
-        <div className={styles.worksListContainer}>
-          <DesignsList />
-        </div>
-      </div>
-      
-      <div className={styles.subworkContainer}>
-        <h2 className={`${styles.worksSubtitle} ${theme === 'dark' ? styles.dark : ''}`}>Illustrations</h2>
-        <div className={styles.worksListContainer}>
-          <IllustrationsList />
-        </div>
+      <div className={styles.display}>
+        {active === "default" && <h2>Select an option!</h2>}
+
+        {active === "Website Development" && <WebProjectsList />}
+        {active === "UI/UX Design" && <DesignsList />}
+        {active === "Art Illustrations" && <IllustrationsList />}
       </div>
     </section>
   );
